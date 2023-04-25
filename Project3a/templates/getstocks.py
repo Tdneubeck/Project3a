29fad7beb6955137aea468639305dcef0e38cd55
Tdneubeck <tdnkvc@umsystem.edu>
2023-04-25 11:48:33 -0500

    code for project3a

---
+++ Project3a/templates/getstocks.py
@@ -0,0 +1,23 @@
+import jinja2
+
+class getstock():
+    
+    def Stockchoice():
+       import csv
+       with open('stocks.csv', newline='') as f:
+          reader = csv.reader(f)
+          data = list(reader)
+          
+       n = 1
+       choicelist = [] 
+       for x in data:
+          for w in x:
+            choicelist.append(w)
+       print(choicelist)
+          
+       return choicelist
+    
+
+    
+    Stockchoice()
+    
