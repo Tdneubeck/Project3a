3e29c6cbb5fd5080935cc7000cd40488455ac960
Tdneubeck <tdnkvc@umsystem.edu>
2023-04-25 12:05:46 -0500

    working versions

--- Project3a/app.py
+++ Project3a/app.py
@@ -10,37 +10,6 @@ app.config["DEBUG"] = True
 #flash  the secret key to secure sessions
 app.config['SECRET_KEY'] = 'hello'
 
-# Function to open a connection to the database.db file
-def get_db_connection():
-    # create connection to the database
-    conn = sqlite3.connect('database.db')
-    
-    # allows us to have name-based access to columns
-    # the database connection will return rows we can access like regular Python dictionaries
-    conn.row_factory = sqlite3.Row
-
-    #return the connection object
-    return conn
-
-## function to get a post
-def get_post(post_id):
-    #get a database connection
-    conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
-    conn.close()
-
-    if post is None:
-        abort(404)
-    
-    return post
-
-
-# use the app.route() decorator to create a Flask view function called index()
-
-
-#route to edit post
-
-# route to delete a post
 
 
 @app.route('/', methods=('GET', 'POST'))
